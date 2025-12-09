b5ee366e51cdaa07d5f07bfdcba8aad1e1031365
JQnetherlands <jhonnyquebrada@gmail.com>
2025-12-09 12:21:31 +0100

    UPDATE Sentry DSN TO ENV

--- src/instrument.mjs
+++ src/instrument.mjs
@@ -1,8 +1,9 @@
 // Import with `import * as Sentry from "@sentry/node"` if you are using ESM
+import 'dotenv/config';
 import { init } from "@sentry/node";
 
 init({
-  dsn: "https://4eb873d8ff0d388c41e5965648c85c57@o4510115802710016.ingest.de.sentry.io/4510397768663120",
+  dsn: process.env.SENTRY_DSN,
   // Setting this option to true will send default PII data to Sentry.
   // For example, automatic IP address collection on events
   sendDefaultPii: true,
